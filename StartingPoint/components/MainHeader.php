dd1cb1c7fd49c5c3f86ad116cbe2256d762980fe
PedrosoCode <Gabrielpedrofc@gmail.com>
2023-12-08 07:55:07 -0300

    tentando incluir php dinâmico

---
+++ StartingPoint/components/MainHeader.php
@@ -0,0 +1,21 @@
+<!-- header.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Página</title>
+    <link rel="stylesheet" href="styles.css"> <!-- Link para o seu arquivo de estilos CSS -->
+</head>
+<body>
+    <header>
+        <h1>Meu Site</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Página Inicial</a></li>
+                <li><a href="#">Sobre Nós</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
