01d8a35ff8e5340433380128207c47696e88a568
PedrosoCode <Gabrielpedrofc@gmail.com>
2023-12-11 20:06:16 -0300

    navbar e footer funcionais

--- StartingPoint/components/MainHeader.php
+++ StartingPoint/components/MainHeader.php
@@ -1,12 +1,12 @@
-<!-- header.php -->
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Página</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Link para o seu arquivo de estilos CSS -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Bootstrap Template</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+
+  <link rel="stylesheet" href="index.css" />
 </head>
 <body>
     <header>
@@ -19,3 +19,5 @@
             </ul>
         </nav>
     </header>
+</body>
+</html>
